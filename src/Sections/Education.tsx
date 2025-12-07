851180ecd8132f15be0c2cb356cc835dc9c87018
lihasahil <sahilshrestha106@gmail.com>
2025-12-07 15:51:03 +0545

    fix: responsiveness

--- src/Sections/Education.tsx
+++ src/Sections/Education.tsx
@@ -9,7 +9,7 @@ const Education = () => {
       <h1 className="text-center text-2xl font-bold my-8 text-design">
         EDUCATION
       </h1>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 pb-4">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 sm:gap-0 pb-4">
         {/* First card */}
         <Card
           icons="/icons/hcoe.png"
